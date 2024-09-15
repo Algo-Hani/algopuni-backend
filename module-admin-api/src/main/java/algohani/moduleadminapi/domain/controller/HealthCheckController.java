4606581cd689b65477464aed882715c8b3de6d99
Woong99 <dndry6852@gmail.com>
2024-09-15 22:00:38 +0900

    ci/cd: CI/CD 환경 구축
    1. github actions를 활용한 CI/CD 환경 구축
    2. 임시 log4j2.xml 추가
    3. Health Check Endpoint 변경

--- module-admin-api/src/main/java/algohani/moduleadminapi/domain/controller/HealthCheckController.java
+++ module-admin-api/src/main/java/algohani/moduleadminapi/domain/controller/HealthCheckController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/health-check")
+@RequestMapping("/api/health-check")
 @RequiredArgsConstructor
 public class HealthCheckController {
 
--- module-user-api/src/main/java/algohani/moduleuserapi/domain/controller/HealthCheckController.java
+++ module-user-api/src/main/java/algohani/moduleuserapi/domain/controller/HealthCheckController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/health-check")
+@RequestMapping("/api/health-check")
 @RequiredArgsConstructor
 public class HealthCheckController {
 
