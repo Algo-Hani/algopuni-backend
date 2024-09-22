443774c17db3843ab1be61a208d3eabe3e930421
Woong99 <dndry1072@naver.com>
2024-09-22 23:29:35 +0900

    [USER-API] feat: Auditing 수정
    인증 안된 사용자는 anonymousUser 문자열 반환

--- module-user-api/src/main/java/algohani/moduleuserapi/global/db/AuditorAwareImpl.java
+++ module-user-api/src/main/java/algohani/moduleuserapi/global/db/AuditorAwareImpl.java
@@ -10,9 +10,8 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.empty();
+            return Optional.of("anonymousUser");
         }
 
         return Optional.of(authentication.getName());
