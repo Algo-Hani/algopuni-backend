4ea3cfd7e172f842d8c5bf5c296bfa3d1d2defe1
Woong99 <dndry1072@naver.com>
2024-09-25 21:55:38 +0900

    [USER-API] feat: 회원가입 이메일 인증 AWS SES로 변경
    1. 속도 이슈로 GMAIL -> AWS SES로 변경

--- module-user-api/src/main/java/algohani/moduleuserapi/domain/auth/service/EmailService.java
+++ module-user-api/src/main/java/algohani/moduleuserapi/domain/auth/service/EmailService.java
@@ -1,38 +1,95 @@
 package algohani.moduleuserapi.domain.auth.service;
 
 import algohani.common.utils.RandomUtils;
-import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.ses.SesAsyncClient;
+import software.amazon.awssdk.services.ses.model.SendRawEmailRequest;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    @Value("${spring.mail.username}")
-    private String fromEmail;
+    private static final String FROM = "Algopuni <algopuni@mail.algopuni.site>"; // 보내는 사람 이메일
 
-    private final JavaMailSender javaMailSender;
+    private static final String EMAIL_TEMPLATE_PATH = "static/html/signup_email_template.html"; // 이메일 템플릿 경로
 
-    public String sendEmail(final String email) throws MessagingException {
+    private static final String LOGO_PATH = "static/image/logo.png"; // 로고 이미지 경로
+
+    private final SesAsyncClient sesAsyncClient;
+
+    private String emailTemplate; // 이메일 템플릿 캐싱
+
+    public String sendEmail(final String email) throws MessagingException, IOException {
+        // 랜덤 코드 생성
         final String randomCode = RandomUtils.generateRandomString(8);
-        MimeMessage mimeMessage = createEmailForm(email, randomCode);
-        javaMailSender.send(mimeMessage);
+
+        // MimeMessage 생성
+        MimeMessage message = new MimeMessage(Session.getDefaultInstance(new Properties()));
+
+        // MimeMessageHelper 생성
+        createMimeMessageHelper(message, email, randomCode);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        message.writeTo(outputStream);
+
+        ByteBuffer buf = ByteBuffer.wrap(outputStream.toByteArray());
+
+        byte[] arr = new byte[buf.remaining()];
+        buf.get(arr);
+
+        // SendRawEmailRequest 생성
+        SendRawEmailRequest rawEmailRequest = SendRawEmailRequest.builder()
+            .rawMessage(r ->
+                r.data(SdkBytes.fromByteArray(arr)).build()
+            )
+            .build();
+
+        // 이메일 전송
+        sesAsyncClient.sendRawEmail(rawEmailRequest);
 
         return randomCode;
     }
 
-    private MimeMessage createEmailForm(final String email, final String randomCode) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        message.addRecipients(Message.RecipientType.TO, email);
-        message.setSubject("ALGOPUNI 회원가입 인증 코드");
-        message.setFrom(fromEmail);
-        message.setText(randomCode);
+    /**
+     * MimeMessageHelper 생성
+     *
+     * @param message    MimeMessage
+     * @param email      이메일
+     * @param randomCode 인증 코드
+     * @throws MessagingException 메시지 생성 실패
+     * @throws IOException        파일 읽기 실패
+     */
+    private void createMimeMessageHelper(final MimeMessage message, final String email, final String randomCode) throws MessagingException, IOException {
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+        messageHelper.setFrom(FROM);
+        messageHelper.setTo(email);
+        messageHelper.setSubject("ALGOPUNI 회원가입 인증 코드");
+        messageHelper.setText(getEmailTemplate().replace("{{verification_code}}", randomCode), true);
+        messageHelper.addInline("image", new ClassPathResource(LOGO_PATH).getFile());
+    }
 
-        return message;
+    /**
+     * 이메일 템플릿 반환 (캐싱)
+     *
+     * @return 이메일 템플릿 문자열
+     * @throws IOException 파일 읽기 실패
+     */
+    private String getEmailTemplate() throws IOException {
+        if (emailTemplate == null) {
+            emailTemplate = new String(new ClassPathResource(EMAIL_TEMPLATE_PATH).getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        }
+        return emailTemplate;
     }
 }
