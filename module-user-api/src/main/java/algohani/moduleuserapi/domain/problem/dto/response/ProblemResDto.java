8a1465fb7b99552906d5ffbd304a3e5fc7212a26
Woong99 <dndry1072@naver.com>
2024-10-03 21:11:08 +0900

    [USER-API] feat: 문제 상세조회 API 개발

--- module-user-api/src/main/java/algohani/moduleuserapi/domain/problem/dto/response/ProblemResDto.java
+++ module-user-api/src/main/java/algohani/moduleuserapi/domain/problem/dto/response/ProblemResDto.java
@@ -1,5 +1,7 @@
 package algohani.moduleuserapi.domain.problem.dto.response;
 
+import algohani.common.enums.LanguageType;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +14,18 @@ public class ProblemResDto {
         int level,
         int successCount,
         int successRate,
+        boolean isFavorite
+    ) {
+
+    }
+
+    public record RelatedInfo(
+        String title,
+        String description,
+        String restriction,
+        String ioExample,
+        String ioDescription,
+        List<LanguageType> languageTypes,
         boolean isFavorite
     ) {
 
